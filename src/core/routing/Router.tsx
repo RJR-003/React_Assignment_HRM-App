c52212d3e560826586ca16a634be1b625876c803
RISHI J R <rishi@qburst.com>
2023-11-06 16:46:30 +0530

    add deploy.yml

--- src/core/routing/Router.tsx
+++ src/core/routing/Router.tsx
@@ -3,10 +3,13 @@ import Home from "../../pages/home/Home";
 import EmpDetails from "../../pages/emp_Details/EmpDetails";
 import AddUpdateEmp from "../../pages/add_Update_Emp/AddUpdateEmp";
 
-const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/view-employee", element: <EmpDetails /> },
-  { path: "/add-update-employee", element: <AddUpdateEmp /> },
-]);
+const router = createBrowserRouter(
+  [
+    { path: "/", element: <Home /> },
+    { path: "/view-employee", element: <EmpDetails /> },
+    { path: "/add-update-employee", element: <AddUpdateEmp /> },
+  ],
+  { basename: import.meta.env.DEV ? "/" : "/React_Assignment_HRM-App /" }
+);
 
 export default router;
