f9da240ce30d4c77ec6a2616b316deb95ba39b84
RISHI J R <rishi@qburst.com>
2023-11-30 17:57:26 +0530

    test deploy

--- src/core/routing/Router.tsx
+++ src/core/routing/Router.tsx
@@ -5,17 +5,20 @@ import AddUpdateEmp from "../../pages/add_Update_Emp/AddUpdateEmp";
 import Layout from "../../layout/Layout";
 import ErrorPage from "../../pages/errorPage/ErrorPage";
 
-const router = createBrowserRouter([
-  {
-    element: <Layout></Layout>,
-    children: [
-      { path: "/", element: <Home /> },
-      { path: "/view-employee/:id", element: <EmpDetails /> },
-      { path: "/add-employee", element: <AddUpdateEmp /> },
-      { path: "/update-employee/:id", element: <AddUpdateEmp /> },
-    ],
-    errorElement: <ErrorPage />,
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      element: <Layout></Layout>,
+      children: [
+        { path: "/", element: <Home /> },
+        { path: "/view-employee/:id", element: <EmpDetails /> },
+        { path: "/add-employee", element: <AddUpdateEmp /> },
+        { path: "/update-employee/:id", element: <AddUpdateEmp /> },
+      ],
+      errorElement: <ErrorPage />,
+    },
+  ],
+  { basename: import.meta.env.DEV ? "/" : "/React_Assignment_HRM-App/" }
+);
 
 export default router;
